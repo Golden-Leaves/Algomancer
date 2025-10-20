90204618a311a5f70e486979eaa2488fa9d0e20a
Golden_Leaves <letuongminh10@gmail.com>
2025-10-20 18:53:12 +0700

    AlgoScene can now register structures in throuhg a WeakValueDictionary

--- Utils/runtime.py
+++ Utils/runtime.py
@@ -1,8 +1,13 @@
+from __future__ import annotations
 from manim import *
 import contextvars
 import contextlib
 import sys
-
+import weakref
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from Structures.arrays import VisualStructure
+    
 ANIMATION_CONTEXT = contextvars.ContextVar("ANIMATION_CONTEXT", default=False) 
 CURRENT_LINE = contextvars.ContextVar("CURRENT_LINE", default=None)
 @contextlib.contextmanager
@@ -31,8 +36,13 @@ def is_animating() -> bool:
 
 class AlgoScene(Scene):
     """Scene subclass that knows when it's playing an animation."""
+    
     _inside_play_call = False
-
+    def __init__(self, renderer = None, camera_class = None, always_update_mobjects = False, random_seed = None, skip_animations = False):
+        super().__init__(renderer, camera_class, always_update_mobjects, random_seed, skip_animations)
+        self._trace = []
+        self._structures = weakref.WeakValueDictionary()
+        
     @contextlib.contextmanager
     def animation_context(self):
         #Do this so the user doesn't need to import the other one but simply call self.animation_context()
@@ -50,5 +60,9 @@ class AlgoScene(Scene):
     def in_play(self): #convinience lol
         return self._inside_play_call
     
-def get_current_line_metadata():
+    def register_structure(self,structure:VisualStructure) -> None:
+        self._structures[id(structure)] = structure
+        
+def get_current_line_metadata() :
     return CURRENT_LINE.get()
+
