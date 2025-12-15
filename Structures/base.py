d70dc2326e6d30db0b098e9d8a1909fe9d4f515a
Golden_Leaves <letuongminh10@gmail.com>
2025-12-15 20:14:37 +0700

    Modify rect.center center an element

--- Structures/base.py
+++ Structures/base.py
@@ -1,5 +1,5 @@
 from __future__ import annotations
-
+from Components.logging import DebugLogger
 import pygame
 
 
@@ -7,11 +7,13 @@ class VisualElement(pygame.sprite.Sprite):
     """Base class for visual elements rendered with pygame sprites."""
 
     def __init__(
-        self, pos: tuple[float, float], *groups: pygame.sprite.AbstractGroup
+        self, pos: tuple[float, float], width: int, height: int, *groups: pygame.sprite.AbstractGroup
     ) -> None:
         super().__init__(*groups)
+        self.logger = DebugLogger(logger_name=f"{__name__}.{self.__class__.__name__}")
         self.pos: pygame.math.Vector2 = pygame.math.Vector2(pos)
-        self.rect = pygame.Rect(self.pos.x, self.pos.y, 20, 20)
+        self.rect = pygame.Rect(0, 0 , width, height)
+        self.rect.center = (self.pos.x, self.pos.y)
 
     def update(self) -> None:
         """Update the sprite each frame. Subclasses may override as needed."""
