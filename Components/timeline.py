66cf28d35357a75851c5afaf6e031027ec12d76d
Golden_Leaves <letuongminh10@gmail.com>
2025-12-17 20:03:27 +0700

    Switch from perf_counter() time to dt from event.dt

---
+++ Components/timeline.py
@@ -0,0 +1,26 @@
+import time
+from Components.animations import Animation
+class Timeline:
+    def __init__(self):
+        self.active: list[Animation] = []
+        self.elapsed_time:float = 0.0
+    def play(self,animation:Animation | list[Animation]):
+        animations = animation if isinstance(animation, list) else [animation]
+        for animation in animations:
+            animation._start_time = self.elapsed_time
+            self.active.append(animation)   
+        
+    def update(self,dt:float):
+        print("timeline hi")
+        self.elapsed_time += dt
+        finished = []
+        for animation in self.active:
+            elapsed = self.elapsed_time - animation._start_time
+            t = elapsed / animation.duration #alpha(progress)
+            animation.apply(t)
+            if t >= 1.0:
+                finished.append(animation)
+        for finished_animation in finished:
+            self.active.remove(finished_animation)
+            
+            
